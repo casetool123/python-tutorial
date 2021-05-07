30141662d4baa13ed9c564f7b0e2f766f2d20444
Amrita Ghosh <58308399+casetool123@users.noreply.github.com>
2021-05-07 12:19:28 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,18 @@
+# print("Amrita")
+# print("21")
+
+# data type
+# int 
+# String
+# bool
+# float
+
+print("int : 100, 455, 3, 7, 0, 1")
+# string : "name", "abc", "xyz", "234"
+# bool : True - 1, False - 0
+# float : 3.9, 6.9, 1.0, 34.98
+
+print(type(2))
+print(type("Amrita"))
+print(type(True))
+print(type(5.7))
---
+++ variable.py
@@ -0,0 +1,61 @@
+# True, False, if, else, pass, continue, break,
+# try, while, for, etc...
+
+#variable stores a value
+
+name = 'Amrita'
+a = 5
+
+#1. starts with an alphabet or _ and then followed by
+#   0,digits,alphabets,_
+ab20_ = '100'
+_abc = 'amrita'
+#0abc = incorrect
+
+#2. cannot start with a number
+#48y = wrong
+
+#3. special characters are not allowed.
+#@,:,/,%,* = not allowed 
+# a@a = wrong
+# a/b = wrong
+
+#4. keywords not allowed
+#Ture = 'ok' wrong
+#if = 'wrong'
+
+# _variable = '10'
+# @f = 2
+
+name = 'Amrita'
+print(name)
+
+
+# print(a)
+
+b = 2.1
+print(b)
+
+a = 4
+a = 2
+a = 10
+print(a)
+
+abc = "Aaron"
+print(abc)
+abc = 'Akshaj'
+print(abc)
+abc = "Jeremy"
+print(abc)
+
+a=1;b="amrita";c=4.5
+print(a)
+print(b)
+print(c)
+
+
+b = ' everyone'
+print('welcome'+b)
+
+
+
