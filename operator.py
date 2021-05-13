1b44a57b4a19d98bfe9cb30bb2436e5d0771ef44
casetool123 <amritaghosh712000@gmail.com>
2021-05-13 17:24:22 +0530

    chnage

--- operator.py
+++ operator.py
@@ -1,28 +1,71 @@
+
+#ARITHMETIC OPERATORS
 #addition
-print(2+3)
-#substraction
-print(5-3)
-#multiply
-print(2*5)
-#division
-print(6/10)
-#modulus
-print(4%3)
-#exponentiation
-print(2**3)
-#floor division
-# 9/3
-print(9//4)
+# print(2+3)
+# #substraction
+# print(5-3)
+# #multiply
+# print(2*5)
+# #division
+# print(6/10)
+# #modulus
+# print(4%3)
+# #exponentiation
+# print(2**3)
+# #floor division
+# # 9/3
+# print(9//4)
+
+# #RELATIONAL OPERATORS
+# print(5==15)
+# print(32==32)
+# print(6==9)
+# print(2!=3)
+# print(10!=10)
+# print(12!=6)
+# print(15>9)
+# print(5>9)
+# print(200>150)
+# print(3<8)
+# print(13<8)
+# # print(50<89)
+# # print(28>=20)
+# # print(67>=67)
+# # print(5>=20)
+# print(10<=50)
+# print(120<=20)
+# print(8<=8)
+
+# x=2
+# y=3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x%y)
+# print(x**y)
+# print(x//y)
+
+#logical operators
+print(2==2 and  4==4)
+print(2==2 and 4==3)
+print(3>2 and 5>10)
+print(3!=3 and 3>=9)
+
+print(4==4 or 3!=2)
+print(4>=3 or 5>3)
+print(4>=4 or 4<3)
+print(3==6 or 4!=4)
+
+print(not 2==2)
+print(not 3!=3)
+
+#assignment operators
+1. =  eg: x=4, abc = "name", a= 6.5
+2. += eg: x+=4 --> x = x + 4
+3. -= eg: x-=5 --> x = x - 5
+4. *= eg: x*=6 --> x = x * 6
+5. /= eg: x/=2 --> x = x / 2
 
 
-print(26==62)
-print(6==6)
-print(2!=3)
-print(7!=7)
-print(5>9)
-print(3<8)
-print(20>=20)
-print(20>=10)
-print(10<=5)
-print(12>=20)
 
