276a192652382655fe87bf18a80acdafa1cc2e1d
casetool123 <amritaghosh712000@gmail.com>
2021-05-20 19:07:33 +0530

    new

--- expression.py
+++ expression.py
@@ -4,12 +4,72 @@ print(exp1)
 exp2 = 4*2*2
 print(exp2)
 
-exp3= 4+5*2
+# exp3= 4+5*2
+#     = 4+10
+#     = 14
+
+# exp4 = (4+2) * 8 + 1
+#     =  6 * 8 + 1
+#     =  48 + 1
+#     = 49
+
+# exp5 = 8/2 + (5*2)
+#     =  8/2 + 10
+#     =  4 + 10
+#     =  14
+
 
 # When multiple operators are used in an arithmetic expression,
 # it is evaluated on the basis of precedence(priority) of the operators.
 
 # () - parenthesis is having the highest precedence
 # whereas "logical or" is having the lowest precedence.
+
+# 1. 3 + 4 * 2
+#     =3+8 = 11
+
+# 2. 18/2 - 6
+#    =9 - 6= 3
+
+# 3. (4+6) * 7
+#    10 *7 =70
+
+# 4. 10 - 4 /2
+#    = 10- 2 = 8
+
+
+
+#Association - associativity: If there is a tie between operators of same 
+              #precedence, it is evaluated using the property associativity.
+
+# 1. 10*5 +4 /2
+#     = 50 + 4/2
+#     = 50 +2 = 52
+
+# 2. 15/3 - 3 + 6
+#     = 5 - 3 + 6
+#     = 2 + 6 = 8
+
+# 3. 2+4 /2 - 1
+#    = 2+ 2 -1
+#    = 4 -1 = 3
+
+# 4. 3*4/2+5
+#    = 12 / 2 + 5
+#    = 6 + 5 = 11
+
+# 5. (4+2)/2 + 7*2
+#   = 6/2 + 7*2
+#   = 3 + 7 * 2
+#   = 3 + 14 = 17
+   
+# 6. (3-1)*2 + 8/2
+#    =2 * 2 + 8/2
+#    =4 + 8/2
+#    = 4 + 4 = 8
+
+
+x = 3 + 4 * 2
+print(x)
 
 
---
+++ function.py
@@ -0,0 +1,22 @@
+# built-in functions of python
+
+print('abc')
+
+type('abc')
+type(2)
+type(45.6)
+type(True)
+
+#conversions
+print(int(12.2))
+print(float(12))
+
+print(abs(-12))           #absolute
+print(pow(2,3))             #power - 2**3
+print(min(2,1,6,9))           #minimum
+print(max(34, 67, 100, 1))
+
+print(divmod(10, 3))            #division, mod
+print(bin(20))                    #binary- 1, 0
+print(oct(31))                    #octa decimal or octal
+print(hex(20))                     #hexa decimal
