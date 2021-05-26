a31460aead80c629b1e8ed5489ef09f3c40440b8
casetool123 <amritaghosh712000@gmail.com>
2021-05-26 17:22:41 +0530

    new

--- string.py
+++ string.py
@@ -40,6 +40,48 @@ print(s5)
 print("aaaaa")
 print(s1*5)
 
+#slicing
+s5 = "programming python"
+s6 = "COMPUTER"
+#print(variable_name[starting_index : ending_index+1])
+
+print(s5[0:4]) #--- > prog 
+print(s5[8:11]) #-- >ing 
+print(s5[4:6]) #--->ra 
+print(s5[8:14]) #--> ing py
+
+#to find out the length of a string -- len()
+print(len(s5))
+
+#method
+#print(variable_name.method_name())
+#to make a string in uppercase -- upper()
+print(s5.upper())
+print(s6.lower())
+
+#to make the first character to uppercase -- capitalize()
+print(s5.capitalize())
+
+#to count the number of characters -- count()
+s7 = "I am writing a code aa hgvnk kljfc aaa kkutvg am "
+print(s7.count('a'))
+
+# to replace a character in a string -- replace()
+print(s7.replace('w','10'))
+
+
+s = 'The Terrible Tiger Tore The Towel'
+#write a code to count the number of 'T' and replace all the 'T' with 't'
+
+sample = 'I am writing python code. I am a beginner.'
+# write a code to replace a the frst 'am' with 'are'.
+#indexing # slicing #replace() ...
+#output: 'I are writing python code. I am a beginner.'
+
+
+
+
+
 
 
 
