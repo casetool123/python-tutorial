9646df0e26dba5f8b1126ccbafc1f084edc2c405
casetool123 <amritaghosh712000@gmail.com>
2021-06-05 17:46:12 +0530

    new lesson

---
+++ data_type2.py
@@ -0,0 +1,82 @@
+# 1. List
+# list is a data type which is a container of multiple data items. 
+
+num = [10, 25, 30, 40, 100]
+# set(num)
+print(num)
+# #lists can contain dissimilar data types.
+# example = ['Sanjay', 20, 20, 43.6]
+# print(example)
+
+# # lists are mutable - chageable
+# animal = ['Zebra', 'Lion', 'Tiger', 'Elephant']
+# print(animal)
+# animal[0] = 'Fox'
+# print(animal)
+
+# #inserting element in list
+# names = ['Aaron', 'Akshaj', 'Jeremy']
+# print(names)
+# names.insert(1,'Sanjay')
+# print(names)
+
+
+# 2. Tuple
+#tuple is a data type which is a container of multiple data items. 
+# in tuples datas are enclosed within () 
+# tpl = (10,10, 20, 30 ,40)
+# a = (10, 'Sanjay')
+# print(a[0])
+# tuples are immutable
+
+
+# # 3. sets
+# #sets does not contain duplicate items
+#sets are mutable
+st = {12, 34, 45}
+print(st)
+s = list(st)
+print(s)
+
+# st1 = {10, 10, 10}
+# print(st1)
+# st2 = {'abc', 'efg', 'pqr'}
+
+
+
+#4. dictionary
+#  a dictionary is a collection of key-value pair.
+
+# d1= {10:'a', 20:'b', 30:'c'}
+# d2 = {'abc':'okay', 'efg':'Thank u'}
+# d3 = {'a':10, 'b':20, 'c':30}
+# print(d3)
+
+# # dictionaries are mutable
+# d3['d'] = 40
+# print(d3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
