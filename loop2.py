6688fa87d0c91c47e9aa5e42f3a5ffee15093722
casetool123 <amritaghosh712000@gmail.com>
2021-06-10 18:02:36 +0530

    loop2

---
+++ loop2.py
@@ -0,0 +1,60 @@
+# 2. FOR LOOP
+# for loop is used to iterate over elements in a string, tuple, list, dictionary.
+
+# #syntax:
+# for var in list:
+#     statement 1
+#     statement 2
+
+#1. repeat a set of statements finite number of times.
+
+# print(range(500))
+# range(10): 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# range(100) : 0 - 99
+
+# range(10,20) : 10 - 19
+# range(20,40) : 20 - 39
+
+# for i in range(15):
+#     print("running")
+
+# for i in range(5):
+#     print(i)
+
+
+
+#2. for loop is used to iterate(repeat) through a string, list, tuple, set or dictionary.
+
+#string
+# for i in 'dentist':
+#     print(i)
+
+# for i in 'Amrita Ghosh':
+#     print(i)
+
+
+# #list
+# for animal in ['Tiger', 'Lion', 'dog', 'cow']:
+#     print(animal)
+
+# #tuple
+# for flower in ('Rose', 'Lily', 'Lotus'):
+#     print(flower)
+
+#set
+# for number in {2, 7, 6, 90, 45}:
+#     print(number)
+
+
+# #dictionary
+d = {'abc':12, 'efg':15, 'xyz':17}
+# for key in d:
+#     print(key)
+
+
+for key in d.values():
+    print(key)
+
+
+    
+# print({'abc':12, 'efg':15, 'xyz':17})
